8acde0e0e8b220814370b64416d7d236fd14d5da
Juan Camilo Moreno Perez <jmoreno40@unisalle.edu.co>
2026-03-19 02:06:00 -0500

    hacemos el logueo y añadimos erramientas

--- televentasjuan.py
+++ televentasjuan.py
@@ -1,1 +1,23 @@
-print("hola mundo")
+"""
+TeleVentas - Sistema de Compras a Distancia
+estudiante: Juan Moreno
+"""
+ 
+# Importamos las herramientas de phyton que necesitamos
+import logging
+import uuid
+from datetime import datetime
+from enum import Enum
+ 
+ #configuramos el logueo con el siguiente codigo
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] - %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("televentas.log"),
+    ],
+)
+log = logging.getLogger("TeleVentas")
+ 
